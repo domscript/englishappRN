90a01483580ebb1ff62cd1b5fd6aa73bb2a0d7d9
domscript <vladimirndsm@gmail.com>
2023-08-12 18:17:03 +0700

    move roundScore

--- Content/utils/smallFunctions.ts
+++ Content/utils/smallFunctions.ts
@@ -1,4 +1,4 @@
-export function countProgress(str: string): [number, string] {
+export const countProgress = (str: string): [number, string] => {
   let count = 0;
   for (let i = 0; i < str.length; i++) {
     if (str[i] === '1' || str[i] === '3') {
@@ -7,7 +7,7 @@ export function countProgress(str: string): [number, string] {
   }
 
   return [count, str];
-}
+};
 
 export const getRandomInt = (min: number, max: number): number => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -20,4 +20,10 @@ export const shuffleArray = <T>(array: T[]): T[] => {
     [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
+};
+
+export const roundScore = (score: number): string => {
+  const [num, des] = score.toString().split('.');
+  if (!des) return num + '.0';
+  return num + '.' + des[0];
 };
