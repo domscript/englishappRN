fe020182b5e06315615c44d22f84c4745d668034
domscript <vladimirndsm@gmail.com>
2023-09-23 21:50:41 +0700

    from class to func

--- Content/components/MyIcons/Verbs/Empty.tsx
+++ Content/components/MyIcons/Verbs/Empty.tsx
@@ -28,40 +28,36 @@ import Colors from '../../../constants/Colors';
 
 import {VerbProps} from './index';
 
-export default class Empty extends React.Component<VerbProps> {
-  render() {
-    const {positive, stage} = this.props;
-
-    return (
-      <View
-        style={[
-          StyleSheet.absoluteFill,
-          {alignItems: 'center', justifyContent: 'center'},
-        ]}>
-        <Svg
-          width="100%"
-          height="100%"
-          viewBox="0 0 600 600"
-          fill={
-            positive < 3
-              ? Colors.green50
-              : positive > 5
-              ? Colors.amber50
-              : Colors.red50
-          }>
-          <G opacity="0.5">
-            <Circle id="background" cx="300" cy="300" r="300" />
-          </G>
-          {stage === 8 && <Path id="walk-8" d="" fill="black" />}
-          {stage === 7 && <Path id="walk-7" d="" fill="black" />}
-          {stage === 6 && <Path id="walk-6" d="" fill="black" />}
-          {stage === 5 && <Path id="walk-5" d="" fill="black" />}
-          {stage === 4 && <Path id="walk-4" d="" fill="black" />}
-          {stage === 3 && <Path id="walk-3" d="" fill="black" />}
-          {stage === 2 && <Path id="walk-2" d="" fill="black" />}
-          {stage === 1 && <Path id="walk-1" d="" fill="black" />}
-        </Svg>
-      </View>
-    );
-  }
-}
+export const Empty = ({positive, stage}: VerbProps) => {
+  return (
+    <View
+      style={[
+        StyleSheet.absoluteFill,
+        {alignItems: 'center', justifyContent: 'center'},
+      ]}>
+      <Svg
+        width="100%"
+        height="100%"
+        viewBox="0 0 600 600"
+        fill={
+          positive < 3
+            ? Colors.green50
+            : positive > 5
+            ? Colors.amber50
+            : Colors.red50
+        }>
+        <G opacity="0.5">
+          <Circle id="background" cx="300" cy="300" r="300" />
+        </G>
+        {stage === 8 && <Path id="walk-8" d="" fill="black" />}
+        {stage === 7 && <Path id="walk-7" d="" fill="black" />}
+        {stage === 6 && <Path id="walk-6" d="" fill="black" />}
+        {stage === 5 && <Path id="walk-5" d="" fill="black" />}
+        {stage === 4 && <Path id="walk-4" d="" fill="black" />}
+        {stage === 3 && <Path id="walk-3" d="" fill="black" />}
+        {stage === 2 && <Path id="walk-2" d="" fill="black" />}
+        {stage === 1 && <Path id="walk-1" d="" fill="black" />}
+      </Svg>
+    </View>
+  );
+};
--- Content/components/MyIcons/Verbs/index.tsx
+++ Content/components/MyIcons/Verbs/index.tsx
@@ -1,30 +1,30 @@
 import React, {ComponentType, useState, useEffect} from 'react';
 
-import Run from './Run';
-import Walk from './Walk';
-import Love from './Love';
-import Think from './Think';
-import Dance from './Dance';
-import Eat from './Eat';
-import Sit from './Sit';
-import Stand from './Stand';
-import Open from './Open';
-import Close from './Close';
-import Sleep from './Sleep';
-import Drink from './Drink';
-import Ask from './Ask';
-import Read from './Read';
-import Want from './Want';
-import Like from './Like';
-import Study from './Study';
-import Jump from './Jump';
-import Give from './Give';
-import Bring from './Bring';
-import Forget from './Forget';
-import Help from './Help';
-import Pay from './Pay';
-import Lose from './Lose';
-import Buy from './Buy';
+import {Run} from './Run';
+import {Walk} from './Walk';
+import {Love} from './Love';
+import {Think} from './Think';
+import {Dance} from './Dance';
+import {Eat} from './Eat';
+import {Sit} from './Sit';
+import {Stand} from './Stand';
+import {Open} from './Open';
+import {Close} from './Close';
+import {Sleep} from './Sleep';
+import {Drink} from './Drink';
+import {Ask} from './Ask';
+import {Read} from './Read';
+import {Want} from './Want';
+import {Like} from './Like';
+import {Study} from './Study';
+import {Jump} from './Jump';
+import {Give} from './Give';
+import {Bring} from './Bring';
+import {Forget} from './Forget';
+import {Help} from './Help';
+import {Pay} from './Pay';
+import {Lose} from './Lose';
+import {Buy} from './Buy';
 
 export type Stages = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 
@@ -66,8 +66,9 @@ const verbComponents: {[key: string]: ComponentType<any>} = {
   buy: Buy,
 };
 
-export const Verb = ({positive, verb}: VerbMainProps) => {
+export const Verb = React.memo(({positive, verb}: VerbMainProps) => {
   const [stage, setStage] = useState<Stages>(1);
+  // const [stage, setStage] = useState(false);
 
   const VerbComponent = verbComponents[verb];
 
@@ -77,7 +78,7 @@ export const Verb = ({positive, verb}: VerbMainProps) => {
         if (stage < 8) return (prevStage + 1) as Stages;
         return 1 as Stages;
       });
-    }, 125);
+    }, 1250);
     return () => clearTimeout(timeoutId);
   }, [stage]);
 
@@ -86,4 +87,4 @@ export const Verb = ({positive, verb}: VerbMainProps) => {
   }
 
   return <VerbComponent positive={positive} stage={stage} />;
-};
+});
