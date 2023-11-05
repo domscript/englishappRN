5b0cc2d7f8600eb8f39c5da89002eaf28b798499
domscript <vladimirndsm@gmail.com>
2023-11-05 12:34:08 +0700

    improve app screen

---
+++ Content/screens/ImproveApp.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {useSelector} from 'react-redux';
+import {RootState} from '../redux-store/store';
+import Colors from '../constants/Colors';
+import {MemberLinks} from '../components/MemberLinks';
+
+function InfoScreen() {
+  const {isDarkTheme} = useSelector((state: RootState) => state.theme);
+
+  return (
+    <View
+      style={[
+        styles.rootContainer,
+        {
+          backgroundColor: isDarkTheme ? Colors.gray75 : Colors.gray10,
+        },
+      ]}>
+      <Text
+        style={[
+          styles.title,
+          isDarkTheme ? styles.darkTitle : styles.lightTitle,
+          {backgroundColor: isDarkTheme ? Colors.gray80 : Colors.green20},
+        ]}>
+        Buy Pizza = Improve App
+      </Text>
+      <MemberLinks
+        icon={true}
+        links={[
+          {
+            id: 1,
+            title: 'buy me a pizza',
+            link: 'https://www.buymeacoffee.com/domscript',
+            description: 'buy me a coffee com',
+          },
+        ]}
+      />
+    </View>
+  );
+}
+
+export default InfoScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  title: {
+    height: 60,
+    width: '100%',
+    paddingVertical: 15,
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    borderColor: Colors.gray85,
+    borderBottomWidth: 1,
+    marginBottom: 15,
+  },
+  darkTitle: {color: Colors.green5},
+  lightTitle: {color: Colors.green90},
+});
