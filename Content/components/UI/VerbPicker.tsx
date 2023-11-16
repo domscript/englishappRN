e1678a3455ce5dfb7bf4ac723eb4e7f5bdec79ab
domscript <vladimirndsm@gmail.com>
2023-11-16 08:13:19 +0700

    size (Google recommendation)

--- Content/components/UI/VerbPicker.tsx
+++ Content/components/UI/VerbPicker.tsx
@@ -118,6 +118,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 3,
   },
   modalContainer: {
     flex: 3,
